7759909e803e621c6755ac86ee8a1dedd7465f6e
unknown <chandansagar982@gmail.com>
2024-05-10 21:57:04 +0530

    landing page view

--- src/layouts/landing.js
+++ src/layouts/landing.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar";
+import Inventory from "./scenes/inventory";
+import Branding from "../components/branding";
+import { setProducts } from "../state/index";
+import { UseDispatch, useDispatch, useSelector } from "react-redux";
 
 const Landing = () => {
+  const stagedProducts = useSelector((state) => state.authAndProducts.stage);
+  const sessionToken = sessionStorage.getItem("token");
+  console.log(sessionToken);
+  const dispatch = useDispatch();
+  const [isSearching, setIsSearching] = useState(false);
+
   return (
-    <div className=" h-screen w-screen bg-custom-blue font-sans p-2 overflow-y-auto">
-      <Navbar />
-      outlet
+    <div
+      className={` h-screen w-screen font-sans bg-landing-color overflow-y-auto bg-opacity:${stagedProducts}?"0.5":"1"`}>
+      <Navbar isSearching={isSearching} setIsSearching={setIsSearching} />
+      <Branding />
+
+      <Inventory isSearching={isSearching} setIsSearching={setIsSearching} />
       <Outlet />
     </div>
   );
